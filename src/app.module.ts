8bfbe375a4ac0085a79f3f8ee6bd795c39547c84
Frank Feitosa <feitosfrank22@gmail.com>
2025-09-01 15:30:25 -0300

    banco de dados preparado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CursoService } from './curso/curso.service';
+import { CursoController } from './curso/curso.controller';
+import { CursoModule } from './curso/curso.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CursoModule],
+  controllers: [AppController, CursoController],
+  providers: [AppService, CursoService],
 })
 export class AppModule {}
